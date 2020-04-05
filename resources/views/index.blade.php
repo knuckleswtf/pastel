18adf0b0d10b97c2121bdfc21173fb5de602a1d2
shalvah <diakon.ng@gmail.com>
2020-04-05 13:19:21 +0100

    Fix null error

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title>{{$page['title']}}</title>
+    <title>{{$page['title'] ?? '' }}</title>
 
     <link rel="stylesheet" href="css/style.css" />
     <script src="js/all.js"></script>
